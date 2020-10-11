8608962d196a1d76237f4f8dc45efa376d5418e2
AlainDevis <devisalain04@gmail.com>
2020-10-11 23:33:22 +0200

    styling the footer section

--- Js/index.js
+++ Js/index.js
@@ -7,46 +7,46 @@ function Order(size,crust,toppings){
     if ( size == "Small Size"){
         this.sizePrice = 2000;
     }
-    else if ( size == "Medium Size"){
+    if ( size == "Medium Size"){
         this.sizePrice = 4000;
     }
-    else if ( size == "Large Size"){
+    if ( size == "Large Size"){
         this.sizePrice = 6000;
     }
-    else if ( crust == "Crispy"){
+    if ( crust == "Crispy"){
         this.crustPrice = 500;
     }
-    else if ( crust == "Stuffed"){
+    if ( crust == "Stuffed"){
         this.crustPrice = 800;
     }
-    else if ( crust == "Gluten-free"){
+    if ( crust == "Gluten-free"){
         this.crustPrice = 1000;
     }
-    else if ( toppings == "BBQ sauce, grilled chicken, scallions, and cheddar cheese"){
+    if ( toppings == "BBQ sauce, grilled chicken, scallions, and cheddar cheese"){
         this.toppingsPrice = 1000;
     }
-    else if ( toppings == "Corn and spicy Italian salami"){
+    if ( toppings == "Corn and spicy Italian salami"){
         this.toppingsPrice = 1200;
     }
-    else if ( toppings == "Spinach, caramelized onion, and bacon"){
+    if ( toppings == "Spinach, caramelized onion, and bacon"){
         this.toppingsPrice = 1500;
     }
-    else if ( toppings == "Alfredo sauce, shrimp, and tomatoes"){
+    if ( toppings == "Alfredo sauce, shrimp, and tomatoes"){
         this.toppingsPrice = 800;
     }
-    else if ( toppings == "Canned tuna fish, red onions, and black olives"){
+    if ( toppings == "Canned tuna fish, red onions, and black olives"){
         this.toppingsPrice = 1000;
     }
-    else if ( toppings == "Sardines, red onions, and black olives"){
+    if ( toppings == "Sardines, red onions, and black olives"){
         this.toppingsPrice = 1100;
     }
-    else if ( toppings == "Fried eggplant slices and black olives"){
+    if ( toppings == "Fried eggplant slices and black olives"){
         this.toppingsPrice = 500;
     }
-    else if ( toppings == "Spinach, artichoke hearts, garlic, and red onion"){
+    if ( toppings == "Spinach, artichoke hearts, garlic, and red onion"){
         this.toppingsPrice = 600;
     }
-    else if ( toppings == "Goat cheese with arugula and red onion"){
+    if ( toppings == "Goat cheese with arugula and red onion"){
         vthis.toppingsPrice = 800;
     }
     // this.total = parseInt(this.sizePrice) + parseInt(this.crustPrice) + parseInt(this.toppingsPrice);
@@ -56,15 +56,6 @@ Order.prototype.totalPrice = function() {
     return this.sizePrice + this.crustPrice + this.toppingsPrice;
 }
 
-// function ordercrust(crust){
-//     this.crust = crust;
-// }
-
-// function ordertoppings(toppings = []){
-//     this.toppings = toppings;
-// }
-
-
 //user interface logic
 $(document).ready(function() {
     $("form#new-order").submit(function(event) {
@@ -73,14 +64,14 @@ $(document).ready(function() {
         var toppingPizza = $("select#toppings").val();
         //alert(sizePizza + " " + crustPizza + " " + toppingPizza);
         var newOrder = new Order(sizePizza,crustPizza,toppingPizza);
-        alert(newOrder.toppingsPrice);
+        // alert(newOrder.totalPrice());
 
         $("#show-order").show();
         $("#show-order h2").text("Your Order");
         $(".pizza-size").text(newOrder.size +" "+ newOrder.sizePrice + "rwf");
         $(".pizza-crust").text(newOrder.crust +" "+ newOrder.crustPrice + "rwf" );
         $(".pizza-toppings").text(newOrder.toppings +" "+ newOrder.toppingsPrice + "rwf");
-
+        $(".pizza-total-amount").text(newOrder.totalPrice() + "rwf");
          event.preventDefault();
     });
 });
